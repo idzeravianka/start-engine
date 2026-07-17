1f3e22ac1a8a81cca4d0fdb4c9a4a8cae76554b6
Nobody Knows <nb6.knws@gmail.com>
2026-07-17 19:39:58 +0300

    init commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+// Replace 'your-repo-name' with your exact GitHub repository name
+const repoName = 'start-engine';
+
 const nextConfig: NextConfig = {
+    output: 'export',
+    basePath: isProd ? `/${repoName}` : '',
+    images: {
+        unoptimized: true,
+    },
     experimental: {
         viewTransition: true,
     },
