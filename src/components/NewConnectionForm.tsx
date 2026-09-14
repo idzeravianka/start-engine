6988dfb7f70700cdf52aa93c720dc99615d4e95e
Nobody Knows <nb6.knws@gmail.com>
2026-09-14 13:23:44 +0300

    fixes

--- src/components/NewConnectionForm.tsx
+++ src/components/NewConnectionForm.tsx
@@ -115,7 +115,7 @@ export default function NewConnectionForm({
                         id={key}
                         name={key}
                         type={key === 'pass' ? 'password' : 'text'}
-                        autoComplete={key === 'pass' ? 'current-password' : 'off'}
+                        autoComplete={key === 'pass' ? 'one-time-code' : 'off'}
                         label={FORM_FIELDS_CONFIG[key].label}
                         placeholder={FORM_FIELDS_CONFIG[key].placeholder}
                         value={formik.values[key] ?? ''}
